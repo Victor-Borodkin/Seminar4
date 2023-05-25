ddc3f44c3ed2e30bdad09210f13eee915ba392c2
Victor <44tse@mail.ru>
2023-05-25 20:36:36 +0300

    Решили домашнее задание по 4 семинару

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+// System.Console.WriteLine("Введите 2 числа: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int mult = num;
+// for (int i = 2; i <= num2; i++)
+// {
+//     num *= mult;
+// }
+// System.Console.WriteLine($"Получится: {num} ");
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// int Sum(int number)
+// {   int count = 0;
+//     while (number != 0)
+//     {
+//         int digit = number%10;
+//         count += digit;
+//         number = number/10;
+//     }
+//     return count;
+// }
+
+// System.Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = Sum(num);
+// System.Console.WriteLine($"Сумма цифр в числе {num} = {result}");
+
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int[] Array = new int[8];
+
+// for (int i = 0; i < 8; i++)
+// {
+//     Array[i] = new Random().Next(1, 10);
+//     Console.Write(Array[i] + " ");
+// }
+
